34c904190004f4145f2c27f04e525ed1373960e5
Nitya-Kalloli <nityakalloli.dev@gmail.com>
2024-09-08 11:27:13 +0530

    Connecting with appwrite

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,9 +1,13 @@
+"use server"
 import { users } from "../appwrite.config"
 import { ID,Query } from "node-appwrite";
-// import { parseStringify } from "../utils";
+  import { parseStringify } from "../utils";
+
+
+
+
 export const createUser = async (user: CreateUserParams) => {
     try {
-      // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
       const newuser = await users.create(
         ID.unique(),
         user.email,
@@ -11,17 +15,16 @@ export const createUser = async (user: CreateUserParams) => {
         undefined,
         user.name
       );
-  
-      // return parseStringify(newuser);
+     return parseStringify(newuser);
     } catch (error: any) {
       // Check existing user
       if (error && error?.code === 409) {
-        const existingUser = await users.list([
+        const documents = await users.list([
           Query.equal("email", [user.email]),
         ]);
   
-        return existingUser.users[0];
+        return documents?.users[0];
       }
-      console.error("An error occurred while creating a new user:", error);
+      // console.error("An error occurred while creating a new user:", error);
     }
   };
