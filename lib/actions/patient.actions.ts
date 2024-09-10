ee090a03fb4890934dc20cf0fb3d9367d6333271
Nitya-Kalloli <nityakalloli.dev@gmail.com>
2024-09-10 21:54:24 +0530

    Registration page

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,9 +1,18 @@
 "use server"
-import { users } from "../appwrite.config"
+// import { users } from "../appwrite.config"
 import { ID,Query } from "node-appwrite";
   import { parseStringify } from "../utils";
 
-
+  import {
+    BUCKET_ID,
+    DATABASE_ID,
+    ENDPOINT,
+    PATIENT_COLLECTION_ID,
+    PROJECT_ID,
+    databases,
+    storage,
+    users,
+  } from "../appwrite.config";
 
 
 export const createUser = async (user: CreateUserParams) => {
@@ -27,4 +36,35 @@ export const createUser = async (user: CreateUserParams) => {
       }
       // console.error("An error occurred while creating a new user:", error);
     }
-  };
+};
+// GET USER
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the user details:",
+      error
+    );
+  }
+};
+  
+// GET PATIENT
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", [userId])]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the patient details:",
+      error
+    );
+  }
+};
